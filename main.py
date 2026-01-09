da610749b6c3817c36817c39718ff6a71f97a7c4
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-09 18:30:33 +0000

    Add LED blink
    Added PWMLED for LED control and updated function calls to include it.

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from ollama import ChatResponse
 from os import system
 import soundfile as sf
 import stt_lib
-from gpiozero import Button
+from gpiozero import Button, PWMLED
 
 button = Button(23)
+led = PWMLED(25)
+
+model = 'exaone3.5:2.4b' #'qwen3:1.7b' 
 
 from kittentts import KittenTTS
 m = KittenTTS("KittenML/kitten-tts-nano-0.2")
 
-stt_lib.init(button)
+stt_lib.init(button, led)
 
 response = generate(
-    model='exaone3.5:2.4b',
+    model=model,
     prompt='Hello!',
     keep_alive=-1
 )
@@ -30,7 +33,7 @@ def llm(prompt):
         'role': 'user',
         'content': question,
     })
-    response: ChatResponse = chat(model='exaone3.5:2.4b', messages=messages, think=False)
+    response: ChatResponse = chat(model=model, messages=messages, think=False)
     
     messages.append({
         'role': 'assistant',
@@ -46,8 +49,10 @@ def tts(text):
     sf.write('output.wav', audio, 24000)
     system("pw-play output.wav")
 
+
 print("Ready")
 system("pw-play startup.mp3")
+
 while True:
     button.wait_for_press()
     
@@ -57,7 +62,9 @@ while True:
     print(question)
     
     print("Thinking...")
+    led.blink()
     llm_response = llm(question)
+    led.off()
     
     print("Speaking...")
     tts(".."+ llm_response + "..")
